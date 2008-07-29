e920ae523b4f6c100ede6997f7e374e2f628715a
taylan <taylan@70abd003-ad45-0410-924e-96c0eacb4ef2>
2008-07-29 01:56:58 +0000

    Initial import for Grand Calabash project.
    git-svn-id: http://svn.taylanpince.com/GrandCalabash/trunk@126 70abd003-ad45-0410-924e-96c0eacb4ef2

---
+++ build/site/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+
+<html lang="en">
+<head>
+	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	
+	<title>Grand Calabash</title>
+	
+	<style type="text/css" media="all">
+        @import "css/reset.css";
+	    @import "css/screen.css";
+	</style>
+	
+	<style type="text/css" media="print">
+	    @import "css/print.css";
+	</style>
+	
+	<!--[if lte IE 6]>
+	    <style type="text/css" media="all">
+	        @import "css/ie.css";
+	    </style>
+	<![endif]-->
+	
+</head>
+<body>
+
+
+
+</body>
+</html>
